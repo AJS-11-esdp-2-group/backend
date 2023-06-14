d05fab16cafad2290d4e8fda5251d6998e5a236f
Moon Natalia <nataliya.moon@gmail.com>
2023-06-14 07:03:40 +0600

    #create login/logout on backend: авторизация и logout пользователя

--- flower-shop-backend/src/controllers/users.controller.ts
+++ flower-shop-backend/src/controllers/users.controller.ts
@@ -1,6 +1,6 @@
 import express, { Request, Router, Response } from 'express';
 import db from '@src/db/db';
-import UserSchema, {Users} from '@src/models/users.model';
+import UserSchema, { Users } from '@src/models/users.model';
 import validate from '@src/middlewares/validateRequest';
 
 const controller: Router = express.Router();
@@ -21,21 +21,21 @@ controller.post('/', validate(UserSchema), async (req: Request, res: Response) =
 
         const user = await db.query('SELECT * FROM users WHERE username = $1', [username]);
         const mail = await db.query('SELECT * FROM users WHERE email = $1', [email]);
-        
-        if(user.rows.length > 0 && mail.rows.length > 0) {
-            return res.status(400).send({ 
-                errors: {username: 'User allready exist!', email: 'E-mail allready taken'} 
-            });
-        } 
 
-        if(user.rows.length > 0) {
-            return res.status(400).send({errors: 'User allready exist!'});
+        if (user.rows.length > 0 && mail.rows.length > 0) {
+            return res.status(400).send({
+                errors: { username: 'User allready exist!', email: 'E-mail allready taken' }
+            });
+        }
+
+        if (user.rows.length > 0) {
+            return res.status(400).send({ errors: 'User allready exist!' });
         }
 
         if (mail.rows.length > 0) {
-            return res.status(400).send({errors: 'E-mail allready taken'});
+            return res.status(400).send({ errors: 'E-mail allready taken' });
         }
-        
+
         const date = new Date().toISOString();
 
         const newUser = await db.query(
@@ -57,11 +57,54 @@ controller.post('/', validate(UserSchema), async (req: Request, res: Response) =
                 date
             ]
         );
-       
+
         res.status(200).send(newUser.rows);
     } catch (error) {
         res.status(500).send({ error: error.message });
-    }  
+    }
 });
+
+controller.post('/login', async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body as Users;
+
+        const user = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+
+        if (user.rows.length === 0) {
+            return res.status(401).send({ error: 'Invalid username or password' });
+        }
+
+        const passwordMatches = await db.query(
+            'SELECT crypt($1, $2) = $2 AS password_matches',
+            [password, user.rows[0].password]
+        );
+
+        if (!passwordMatches.rows[0].password_matches) {
+            return res.status(400).send({ errors: 'Invalid username or password' });
+        }
+
+        res.status(200).send({ message: 'Login successful' });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+const invalidTokens = new Set();
+controller.delete('/logout', async (req, res) => {
+    try {
+        const token = req.headers.authorization;
+
+        if (!token) {
+            return res.status(401).send({ error: 'Unauthorized' });
+        }
+
+        invalidTokens.add(token);
+
+        res.status(200).send({ message: 'Logout successful' });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
 
 export default controller;
