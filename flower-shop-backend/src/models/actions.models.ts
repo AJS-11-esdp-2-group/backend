15b600ef062c394bfec685d0ad3aa20c7116a3d9
Daryn <batyrerlik@gmail.com>
2023-06-18 01:34:27 +0600

    разрабоатна модель actions для прихода товаров

---
+++ flower-shop-backend/src/models/actions.models.ts
@@ -0,0 +1,17 @@
+import { string, object, number, InferType, date } from "yup";
+
+const ActionsSchema = object({
+  operation_type_id: number().required('Operation is required'),
+  source_id: number(),
+  target_id: number(),
+  item_id: number(),
+  qty: number(),
+  price: number(),
+  total_price: number(),
+  date: date().required('Create date is required'),
+  update_date: date(),
+  user_id: number()
+});
+
+export type Actions = InferType<typeof ActionsSchema>;
+export default ActionsSchema;
