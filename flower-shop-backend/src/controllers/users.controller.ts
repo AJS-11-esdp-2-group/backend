8d82ec68f3e3c4d0cd26f701a5babdcf354fe2e4
Daryn <batyrerlik@gmail.com>
2023-06-24 09:07:41 +0600

    доработано login  в части изменения select по получению данных

--- flower-shop-backend/src/controllers/users.controller.ts
+++ flower-shop-backend/src/controllers/users.controller.ts
@@ -1,100 +1,111 @@
-import express, { Request, Router, Response } from 'express';
-import db from '@src/db/db';
-import UserSchema, { Users } from '@src/models/users.model';
-import validate from '@src/middlewares/validateRequest';
-import {nanoid} from 'nanoid';
+import express, { Request, Router, Response } from "express";
+import db from "@src/db/db";
+import UserSchema, { Users } from "@src/models/users.model";
+import validate from "@src/middlewares/validateRequest";
+import { nanoid } from "nanoid";
 
 const controller: Router = express.Router();
 
-controller.post('/', validate(UserSchema), async (req: Request, res: Response) => {
+controller.post(
+  "/",
+  validate(UserSchema),
+  async (req: Request, res: Response) => {
     try {
-        const {
-            username,
-            password,
-            email,
-            phone,
-            first_name,
-            last_name,
-            address,
-            country,
-            city
-        } = req.body as Users;
+      const {
+        username,
+        password,
+        email,
+        phone,
+        first_name,
+        last_name,
+        address,
+        country,
+        city
+      } = req.body as Users;
 
-        const user = await db.query('SELECT * FROM users WHERE username = $1', [username]);
-        const mail = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+      const user = await db.query("SELECT * FROM users WHERE username = $1", [
+        username
+      ]);
+      const mail = await db.query("SELECT * FROM users WHERE email = $1", [
+        email
+      ]);
 
-        if (user.rows.length > 0 && mail.rows.length > 0) {
-            return res.status(400).send({
-                errors: { username: 'User allready exist!', email: 'E-mail allready taken' }
-            });
-        }
+      if (user.rows.length > 0 && mail.rows.length > 0) {
+        return res.status(400).send({
+          errors: {
+            username: "User allready exist!",
+            email: "E-mail allready taken"
+          }
+        });
+      }
 
-        if (user.rows.length > 0) {
-            return res.status(400).send({ errors: 'User allready exist!' });
-        }
+      if (user.rows.length > 0) {
+        return res.status(400).send({ errors: "User allready exist!" });
+      }
 
-        if (mail.rows.length > 0) {
-            return res.status(400).send({ errors: 'E-mail allready taken' });
-        }
+      if (mail.rows.length > 0) {
+        return res.status(400).send({ errors: "E-mail allready taken" });
+      }
 
-        const date = new Date().toISOString();
+      const date = new Date().toISOString();
 
-        const newUser = await db.query(
-            `INSERT INTO users
+      const newUser = await db.query(
+        `INSERT INTO users
             (username, password, email, email_verificated, phone, first_name, last_name, address, id_country, id_city, create_date)
             VALUES ($1, crypt($2, gen_salt('bf')), $3, $4, $5, $6, $7, $8, $9, $10, $11)
             RETURNING username, email, phone, first_name, last_name, address, create_date`,
-            [
-                username,
-                password,
-                email,
-                false,
-                phone,
-                first_name,
-                last_name,
-                address,
-                country,
-                city,
-                date
-            ]
-        );
-
-        res.status(200).send(newUser.rows);
+        [
+          username,
+          password,
+          email,
+          false,
+          phone,
+          first_name,
+          last_name,
+          address,
+          country,
+          city,
+          date
+        ]
+      );
+      res.status(200).send(newUser.rows);
     } catch (error) {
-        res.status(500).send({ error: error.message });
+      res.status(500).send({ error: error.message });
     }
-});
+  }
+);
 
-controller.post('/login', async (req: Request, res: Response) => {
-    try {
-        const { username, password } = req.body as Users;
+controller.post("/login", async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body as Users;
 
-        const user = await db.query('SELECT * FROM users WHERE username = $1', [username]);
-        
-        if (user.rows.length === 0) {
-            return res.status(401).send({ error: 'Username not found!' });
-        }
+    const user = await db.query("SELECT * FROM users WHERE username = $1", [
+      username
+    ]);
 
-        const passwordMatches = await db.query(
-            'SELECT crypt($1, $2) = $2 AS password_matches',
-            [password, user.rows[0].password]
-        );
+    if (user.rows.length === 0) {
+      return res.status(401).send({ error: "Username not found!" });
+    }
 
-        if (!passwordMatches.rows[0].password_matches) {
-            return res.status(400).send({ errors: 'Wrong password' });
-        }
+    const passwordMatches = await db.query(
+      "SELECT crypt($1, $2) = $2 AS password_matches",
+      [password, user.rows[0].password]
+    );
 
-        const token = nanoid();
-        const userSetToken = await db.query(
-            `UPDATE users SET 
+    if (!passwordMatches.rows[0].password_matches) {
+      return res.status(400).send({ errors: "Wrong password" });
+    }
+    const token = nanoid();
+    const userSetToken = await db.query(
+      `UPDATE users SET 
                     token = $1
                     WHERE username = $2
                     RETURNING *`,
-            [token, username]
-        );
+      [token, username]
+    );
 
-        const authorizedUser = await db.query(
-            `select
+    const authorizedUser = await db.query(
+      `select
             u.id,
             u.username,
             u.token,
@@ -111,49 +122,46 @@ controller.post('/login', async (req: Request, res: Response) => {
             u.create_date,
             u.last_update_date
             from users u
-            inner join user_roles r on r.id = u.id_role
-            inner join countries coun on coun.id = u.id_country
-            inner join cities cty on cty.id = u.id_city
+            left join user_roles r on r.id = u.id_role
+            left join countries coun on coun.id = u.id_country
+            left join cities cty on cty.id = u.id_city
             where u.username = $1`,
-            [username]
-        );
-
-        res.status(200).send(authorizedUser.rows);
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
+      [username]
+    );
+    res.status(200).send(authorizedUser.rows);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
-controller.delete('/logout', async (req, res) => {
-    try {
-        const token = req.headers.authorization;
+controller.delete("/logout", async (req, res) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).send({ error: "Unauthorized" });
+    }
 
-        if (!token) {
-            return res.status(401).send({ error: 'Unauthorized' });
-        }
+    const user = await db.query(`SELECT * from users WHERE token = $1`, [
+      token
+    ]);
 
-        const user = await db.query(
-            `SELECT * from users WHERE token = $1`, [token]
-        );
-        
-        if(user.rowCount === 0) {
-            return res.status(200).send({message: 'Logout successfull!'})
-        }
+    if (user.rowCount === 0) {
+      return res.status(200).send({ message: "Logout successfull!" });
+    }
 
-        const dropToken = null;
-        const resetToken = await db.query(
-            `UPDATE users SET 
+    const dropToken = null;
+    const resetToken = await db.query(
+      `UPDATE users SET 
                     token = $1
                     WHERE token = $2
                     RETURNING *`,
-            [dropToken, token]
-        );
-        
-        res.status(200).send({ message: 'Logout successful' });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-});
+      [dropToken, token]
+    );
 
+    res.status(200).send({ message: "Logout successful" });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
 
 export default controller;
