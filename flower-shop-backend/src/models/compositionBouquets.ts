34854e0dc0fdd5330eccc180c77b7ddc3d78764c
muhtar-88 <muha774@gmail.com>
2023-06-30 20:48:36 +0600

    #backend/bouquets-controller-upgrade: CRUD по  композициям букетов

--- flower-shop-backend/src/models/compositionBouquets.ts
+++ flower-shop-backend/src/models/compositionBouquets.ts
@@ -3,8 +3,7 @@ import { object, InferType, number } from 'yup';
  const CompositionBouquetsSchema = object({
    id_bouquet: number().required('Id of bouquet is required'),
    id_item: number().required('Id of item is required'),
-   qty: number().required('Qty is required'),
-   price: number()
+   qty: number().required('Qty is required')
  });
 
  export type CompositionBouquets = InferType<typeof CompositionBouquetsSchema>;
