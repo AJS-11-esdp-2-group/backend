ad9e276dc3bc2fcfbff2398fa1ea844010f85abd
muhtar-88 <muha774@gmail.com>
2023-06-30 19:16:10 +0600

    #backend/bouquets-controller-upgrade: CRUD для основным данным букета и изображения для букетов

--- flower-shop-backend/src/controllers/bouquets.controller.ts
+++ flower-shop-backend/src/controllers/bouquets.controller.ts
@@ -8,11 +8,9 @@ const controller: Router = express.Router();
 controller.get('/', async (req: Request, res: Response) => {
     try {
         const bouquets = await db.query(`
-      SELECT 
-        b.id, 
-        b.bouquet_name, 
-        b.bouquet_description
+      SELECT b.id, b.bouquet_name, b.bouquet_description, i.image
       FROM bouquets b
+      JOIN bouquets_images i ON i.id_bouquet = b.id
     `);
         res.status(200).send(bouquets.rows);
     } catch (error) {
@@ -26,8 +24,27 @@ controller.post('/', validate(BouquetSchema), async (req: Request, res: Response
         const newBouquet = await db.query(`
       INSERT INTO bouquets (bouquet_name, bouquet_description)
       VALUES ($1, $2)
-      RETURNING *
+      RETURNING id, bouquet_name, bouquet_description
     `, [bouquet_name, bouquet_description]);
+
+        res.status(200).send(newBouquet.rows[0]);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+controller.put('/:id', validate(BouquetSchema), async (req: Request, res: Response) => {
+  try {
+    const {id} = req.params;
+    const { bouquet_name, bouquet_description } = req.body as Bouquet;
+
+    const newBouquet = await db.query(`
+      UPDATE bouquets SET
+      bouquet_name = $1,
+      bouquet_description = $2
+      WHERE id = $3
+      RETURNING id, bouquet_name, bouquet_description
+    `, [bouquet_name, bouquet_description, id]);
 
         res.status(200).send(newBouquet.rows[0]);
     } catch (error) {
@@ -45,10 +62,10 @@ controller.delete('/:id', async (req: Request, res: Response) => {
       `, [id]);
   
       if (deletedBouquet.rows.length === 0) {
-        return res.status(404).send('Bouquet not found');
+        return res.status(404).send({error: 'Bouquet not found'});
       }
   
-      res.status(200).send('Bouquet deleted successfully');
+      res.status(200).send({success: 'Bouquet not found'});
     } catch (error) {
       res.status(500).send({ error: error.message });
     }
