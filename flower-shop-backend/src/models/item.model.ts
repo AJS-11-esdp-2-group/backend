1643dc00483870a1507a166c0e5e25347b903778
Баилова Таншолпан <bailovat1@gmail.com>
2023-07-08 12:18:02 +0600

    добавила изменения

--- flower-shop-backend/src/models/item.model.ts
+++ flower-shop-backend/src/models/item.model.ts
@@ -1,15 +1,16 @@
 import { object, string, number, InferType, date } from 'yup';
 
- const ItemSchema = object({
-   item_name: string().required('Name is required').max(55),
-   item_description: string(),
-   id_category: number().required('Goods category is required'),
-   id_subcategory: number().required('Goods subcategory is required'),
-   image_small: string(),
-   image_large: string(),
-   id_user: number().required(),
-   create_date: date()
- });
+const ItemSchema = object({
+  item_name: string().required('Name is required').max(55),
+  item_description: string(),
+  price: number().default(0),
+  id_category: number().required('Goods category is required'),
+  id_subcategory: number().required('Goods subcategory is required'),
+  id_under_subcategory: number(),
+  image_small: string(),
+  id_user: number(),
+  create_date: date(),
+});
 
- export type Items = InferType<typeof ItemSchema>;
- export default ItemSchema;
+export type Items = InferType<typeof ItemSchema>;
+export default ItemSchema;
