22d2253b23df9eda647a4d7cfc315c7816f549a1
Баилова Таншолпан <bailovat1@gmail.com>
2023-07-16 03:23:24 +0600

    изменила схему для actions

--- flower-shop-backend/src/models/actions.models.ts
+++ flower-shop-backend/src/models/actions.models.ts
@@ -1,18 +1,22 @@
-import { string, object, number, InferType, date } from "yup";
+import { string, object, number, InferType, array, date } from 'yup';
+
+const ItemSchema = object({
+  item_id: number().required(),
+  price: number().required(),
+  qty: number().required(),
+});
 
 const ActionsSchema = object({
   operation_type_id: number().required('Operation is required'),
-  source_id: number(),
-  target_id: number(),
-  item_id: number(),
-  qty: number(),
-  price: number(),
-  total_price: number(),
+  invoice_number: string().required(),
+  source_id: number().required(),
+  target_id: number().required(),
+  items: array(ItemSchema).required(),
   date: date(),
   update_date: date(),
-  user_id: number()
+  user_id: number(),
 });
 
-
+export type ItemForActions = InferType<typeof ItemSchema>;
 export type Actions = InferType<typeof ActionsSchema>;
 export default ActionsSchema;
