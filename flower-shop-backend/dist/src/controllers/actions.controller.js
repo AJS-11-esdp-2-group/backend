68aab83b62afafe99409fa644432bad0ad1c2ab3
muhtar-88 <muha774@gmail.com>
2023-07-26 18:20:50 +0600

    deploy error fixing

--- flower-shop-backend/dist/src/controllers/actions.controller.js
+++ flower-shop-backend/dist/src/controllers/actions.controller.js
@@ -270,7 +270,7 @@ controller.get('/invoices', (req, res) => __awaiter(void 0, void 0, void 0, func
      left join suppliers ON actions.source_id = suppliers.id
     left JOIN storages s ON actions.target_id= s.id
     group by invoice_number, suppliers.name_supplier, s.storage
-    order by id`);
+    order by date DESC`);
         res.status(200).send(invoiceNumbers.rows);
     }
     catch (error) {
